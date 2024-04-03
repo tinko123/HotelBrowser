06917f5bedec06284d10583f2dc9337e5c54af21
tink0t0 <tinkogroshev@abv.bg>
2024-04-03 13:57:22 +0300

    adding dbconstext to repository

---
+++ HotelBrowser.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBrowser.Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class
+;
+        IQueryable<T> AllReadOnly<T>() where T : class;
+    }
+}
---
+++ HotelBrowser.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBrowser.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly HotelBrowserDbContext data;
+        public Repository(HotelBrowserDbContext _data)
+        {
+            data = _data;
+        }
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return data.Set<T>();
+        }
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking();
+        }
+    }
+}
