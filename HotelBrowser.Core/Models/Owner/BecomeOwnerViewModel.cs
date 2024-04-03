29457b3462ffc4b0af88a88728454baf4d121b53
tink0t0 <tinkogroshev@abv.bg>
2024-04-03 14:27:22 +0300

    adding ownerViewModel

--- HotelBrowser.Core/Models/Owner/BecomeOwnerViewModel.cs
+++ HotelBrowser.Core/Models/Owner/BecomeOwnerViewModel.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using static HotelBrowser.Infrastructure.Constants.Constants;
 
 namespace HotelBrowser.Core.Models.Owner
 {
     public class BecomeOwnerViewModel
     {
+        [Required(ErrorMessage = RequiredField)]
+        [StringLength(OwnerPhoneMaxLength,
+                       MinimumLength = OwnerPhoneMinLength,
+                       ErrorMessage = StringLengthField)]
+        [Display(Name = "Phone Number")]
+        [Phone]
+
+        public string phoneNumber { get; set; } = null!;
     }
 }
