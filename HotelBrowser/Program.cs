7ff611ca6fadb2463d13c2273e23d9398c45802d
tink0t0 <tinkogroshev@abv.bg>
2024-04-17 15:30:12 +0300

    implementing custom errors and making the home page without carosel

--- HotelBrowser/Program.cs
+++ HotelBrowser/Program.cs
@@ -19,12 +19,13 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
     app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error/500");
+    app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
     app.UseHsts();
 }
 
